a2eba9b5ca63568e632a051f83dc774a4af69e69
Nano-Stark <inventorstark7@gmail.com>
2022-07-08 23:34:49 +0100

    Error correction

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Post from './Post';
-import {db, auth} from './firebase';
-import {makeStyles} from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import { Button, Input } from '@material-ui/core';
-import ImageUpload from './ImageUpload';
-import InstagramEmbed from 'react-instagram-embed';
-
-
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import { db, auth } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
+import InstagramEmbed from "react-instagram-embed";
 
 function getModalStyle() {
   const top = 50;
@@ -24,46 +21,44 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
 }));
 
-
 function App() {
-const classes = useStyles();
-const [modalStyle] = useState(getModalStyle);
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
 
-const [posts, setPosts] = useState([]);
-const [open, setOpen] = useState(false);
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('');
-const [email, setEmail] = useState('');
-const [user, setUser] = useState(null);
-const [openSignIn, setOpenSignIn] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState(false);
 
-
-useEffect(() => {
+  useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         //user has logged in
         console.log(authUser);
         setUser(authUser);
-      }else {
+      } else {
         //use has logged out
         setUser(null);
       }
-    })
+    });
 
     return () => {
       //perform some cleanup activities
       unsubscribe();
-    }
-}, [user, username]);
+    };
+  }, [user, username]);
   /*const [posts, setPosts] = useState([
     {
       username: 'Stark ',
@@ -77,168 +72,194 @@ useEffect(() => {
     },
   ]);*/
 
-useEffect(() => {
-    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      //everytime a new post is added, this code fires
-      setPosts(snapshot.docs.map(doc => ({
-        id: doc.id,
-        post: doc.data()
-      })));
-    })
-}, []);
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        //everytime a new post is added, this code fires
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            post: doc.data(),
+          }))
+        );
+      });
+  }, []);
 
-const signUp = (event) => {
+  const signUp = (event) => {
     event.preventDefault();
 
-    auth.createUserWithEmailAndPassword(email, password)
-    .then((authUser) => {
-      return authUser.user.updateProfile({
-        displayName: username,
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
       })
-    })
-    .catch((error) => alert(error.message));
+      .catch((error) => alert(error.message));
 
     setOpen(false);
-}
+  };
 
-const signIn = (event) => {
-  event.preventDefault();
+  const signIn = (event) => {
+    event.preventDefault();
 
-  auth.signInWithEmailAndPassword(email, password)
-  .catch((error) => alert(error.message))
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
 
-  setOpenSignIn(false);
-}
+    setOpenSignIn(false);
+  };
 
   return (
     <div className="app">
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-         >
-            <div style={modalStyle} className={classes.paper}>
-              <form className='app_signup'>
-              <center>
-                <img
-                  className='app_headerImage'
-                  src={'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'}
-                  alt=""
-                />
-              </center>
-              <Input
-                  placeholder='username'
-                  type='text'
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-                <Input
-                  placeholder='email'
-                  type='text'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input
-                  placeholder='password'
-                  type='password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button type='submit' onClick={signUp}>Sign Up</Button>
-                </form>
-            </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src={
+                  "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                }
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>
+              Sign Up
+            </Button>
+          </form>
+        </div>
       </Modal>
 
       {/*sign in, the first modal above is sign up*/}
-      <Modal
-        open={openSignIn}
-        onClose={() => setOpenSignIn(false)}
-         >
-            <div style={modalStyle} className={classes.paper}>
-              <form className='app_signup'>
-              <center>
-                <img
-                  className='app_headerImage'
-                  src={'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'}
-                  alt=""
-                />
-              </center>
-                <Input
-                  placeholder='email'
-                  type='text'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input
-                  placeholder='password'
-                  type='password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button type='submit' onClick={signIn}>Sign In</Button>
-                </form>
-            </div>
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src={
+                  "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                }
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>
+              Sign In
+            </Button>
+          </form>
+        </div>
       </Modal>
       {/**Header */}
-      <div className='app_header'>
+      <div className="app_header">
         <img
-            className='app_headerImage'
-            src={'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'}
-            alt=""
-          />
+          className="app_headerImage"
+          src={
+            "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          }
+          alt=""
+        />
 
-          {user ? (
+        {user ? (
           <Button onClick={() => auth.signOut()}>Logout</Button>
-          ):(
-            <div className='app_loginContainer'>
-              <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-              <Button onClick={() => setOpen(true)}>Sign Up</Button>
-            </div>     
-          )}  
+        ) : (
+          <div className="app_loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
 
-      <div className='app_posts'>
-        <div className='app_postsLeft'>
-            {/**Post */}
-            {
-              posts.map(({id, post}) => (
-                <Post key={id} postId={id} user={user} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-              ))
-            }
-        </div>
-        <div className='app_postsRight'>
-            <InstagramEmbed
-              url='https://www.instagram.com/p/CPoZhWegC6E/'
-              
-              maxWidth={320}
-              hideCaption={false}
-              containerTagName='div'
-              protocol=''
-              injectScript
-              onLoading={() => {}}
-              onSuccess={() => {}}
-              onAfterRender={() => {}}
-              onFailure={() => {}}
+      <div className="app_posts">
+        <div className="app_postsLeft">
+          {/**Post */}
+          {posts.map(({ id, post }) => (
+            <Post
+              key={id}
+              postId={id}
+              user={user}
+              username={post.username}
+              caption={post.caption}
+              imageUrl={post.imageUrl}
             />
+          ))}
+        </div>
+        <div className="app_postsRight">
+          <InstagramEmbed
+            url="https://instagr.am/p/Zw9o4/"
+            clientAccessToken="123|456"
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+          {/* <InstagramEmbed
+            url="https://www.instagram.com/p/CPoZhWegC6E/"
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          /> */}
 
-            <center>
-                <img
-                  className='app_headerImage'
-                  src={'https://static1.srcdn.com/wordpress/wp-content/uploads/2020/06/Tony-Starks-Iron-Man-Mark-LXXXV-Armor-In-Avengers-Endgame.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5'}
-                  alt=""
-                />
-            </center>
-          </div>
+          {/* <center>
+            <img
+              className="app_headerImage"
+              src={
+                "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/06/Tony-Starks-Iron-Man-Mark-LXXXV-Armor-In-Avengers-Endgame.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5"
+              }
+              alt=""
+            />
+          </center> */}
+        </div>
       </div>
 
       {/**optional user?.displayName */}
       {user?.displayName ? (
-          <ImageUpload username={user.displayName}/>
-      ):(
-          <h3>Sorry you need to login to upload</h3>
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Sorry you need to login to upload</h3>
       )}
-
- 
-
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,93 +1,92 @@
-import React, {useState, useEffect} from 'react'
-import './Post.css'
-import Avatar from '@material-ui/core/Avatar';
-import {db} from './firebase';
-import firebase from 'firebase';
+import React, { useState, useEffect } from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
+import { db } from "./firebase";
+import firebase from "firebase";
 
-function Post({postId, user, username, caption, imageUrl}) {//alternatively i can pass props keyword
-    const [comments, setComments] = useState([]);
-    const [comment, setComment] = useState('');
+function Post({ postId, user, username, caption, imageUrl }) {
+  //alternatively i can pass props keyword
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
 
-    useEffect(() => {
-        let unsubscribe;
-        if (postId) {
-            unsubscribe = db
-            .collection('posts')
-            .doc(postId)
-            .collection('comments')
-            .orderBy('timestamp', 'desc')
-            .onSnapshot((snapshot) => {
-                setComments(snapshot.docs.map((doc) => doc.data()));
-            });
-
-        }
-
-        return () => {
-            unsubscribe();
-        };
-    }, [postId]);
-
-
-    const postComment = (event) => {
-        event.preventDefault();
-
-        db.collection('posts').doc(postId).collection('comments').add({
-            text: comment,
-            username: user.displayName,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
         });
-        setComment('');
     }
 
-    return (
-        <div className='post'>
-            {/**header -> avatar + username */}
-            <div className='post_header'>
-                <Avatar 
-                    className='post_avatar'
-                    alt='Stark'
-                    src={'/static/images/avatar/1.jpg'}
-                />
-                <h3>{username}</h3>
-            </div>
-            {/**image */}
-            <img
-                className='post_image'
-                src={imageUrl}
-                alt=""
-            />
-            {/**username + caption */}
-            <h4 className='post_text'><strong>{username}</strong>{caption}</h4>
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
 
-            <div className='post_comments'>
-                {comments.map((comment) => (
-                    <p>
-                        {/**username and text were what i created as fields in firebase */}
-                        <strong>{comment.username}</strong> {comment.text}
-                    </p>
-                ))}
-            </div>
+  const postComment = (event) => {
+    event.preventDefault();
 
-            {user && (
-            <form className='post_commentBox'>
-                <input 
-                    className='post_input'
-                    type='text' 
-                    placeholder='Add a comment...' 
-                    value={comment}
-                    onChange={e => setComment(e.target.value)}
-                    />
-                <button 
-                    className='post_button'
-                    disabled={!comment}
-                    type='submit'
-                    onClick={postComment}
-                    >Post</button>
-            </form>
-            )}
+    db.collection("posts").doc(postId).collection("comments").add({
+      text: comment,
+      username: user.displayName,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setComment("");
+  };
 
-        </div>
-    )
+  return (
+    <div className="post">
+      {/**header -> avatar + username */}
+      <div className="post_header">
+        <Avatar
+          className="post_avatar"
+          alt="Stark"
+          src={"/static/images/avatar/1.jpg"}
+        />
+        <h3>{username}</h3>
+      </div>
+      {/**image */}
+      <img className="post_image" src={imageUrl} alt="" />
+      {/**username + caption */}
+      <h4 className="post_text">
+        <strong>{username}</strong>
+        {caption}
+      </h4>
+
+      <div className="post_comments">
+        {comments.map((comment, index) => (
+          <p key={index}>
+            {/**username and text were what i created as fields in firebase */}
+            <strong>{comment.username}</strong> {comment.text}
+          </p>
+        ))}
+      </div>
+
+      {user && (
+        <form className="post_commentBox">
+          <input
+            className="post_input"
+            type="text"
+            placeholder="Add a comment..."
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            className="post_button"
+            disabled={!comment}
+            type="submit"
+            onClick={postComment}
+          >
+            Post
+          </button>
+        </form>
+      )}
+    </div>
+  );
 }
 
-export default Post
+export default Post;
